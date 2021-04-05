5215d669fbf9f5a1b8c3f438b1de03c8baf4007e
Jack <jackmayer21@gmail.com>
2021-04-04 17:47:11 -0700

    hookified flipCard

---
+++ src/hooks/useFlip.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function useFlip(initialState = true) {
+  const [isFacingUp, setIsFacingUp] = useState(initialState);
+  const flipCard = () => {
+    setIsFacingUp((isUp) => !isUp);
+  };
+
+  return [isFacingUp, flipCard];
+}
+
+export default useFlip;
